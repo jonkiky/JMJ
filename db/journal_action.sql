f78bd4cc7e4248d4697ddfebd09aabb80d7c285e
yizhen <jonkiky@gmail.com>
2018-11-28 23:14:04 -0500

    add database

---
+++ db/journal_action.sql
@@ -0,0 +1,18 @@
+﻿# Host: localhost  (Version 5.7.24-log)
+# Date: 2018-11-28 23:13:01
+# Generator: MySQL-Front 6.1  (Build 1.26)
+
+
+#
+# Structure for table "journal_action"
+#
+
+DROP TABLE IF EXISTS `journal_action`;
+CREATE TABLE `journal_action` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `action_name` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `project_id` varchar(255) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
---
+++ db/journal_data.sql
@@ -0,0 +1,33 @@
+﻿# Host: localhost  (Version 5.7.24-log)
+# Date: 2018-11-28 23:13:12
+# Generator: MySQL-Front 6.1  (Build 1.26)
+
+
+#
+# Structure for table "journal_data"
+#
+
+DROP TABLE IF EXISTS `journal_data`;
+CREATE TABLE `journal_data` (
+  `Journal_id` int(11) NOT NULL AUTO_INCREMENT,
+  `topic` varchar(45) DEFAULT NULL,
+  `User_id` varchar(45) DEFAULT NULL,
+  `progress_start_time` varchar(45) DEFAULT NULL,
+  `progress_peers` longtext,
+  `new_knowledge` longtext,
+  `plan_start_time` varchar(45) DEFAULT NULL,
+  `issues` longtext,
+  `actions` longtext,
+  `planpeers` longtext,
+  `resources` text,
+  `sharepeers` longtext,
+  `permission` varchar(45) DEFAULT NULL,
+  `status` varchar(255) DEFAULT 'Active',
+  `plan_end_time` varchar(255) DEFAULT NULL,
+  `progress_end_time` varchar(255) DEFAULT NULL,
+  `creation_time` varchar(255) DEFAULT NULL,
+  `progress_time_select` varchar(255) DEFAULT NULL,
+  `plan_time_select` varchar(255) DEFAULT NULL,
+  `project_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Journal_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8;
